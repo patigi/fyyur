72f84f45631bf963fc573a7833d4f679883e48a1
Ahmed Abdulrahman <ahmedpatigi@gmail.com>
2022-09-01 10:42:50 +0100

    updated

--- artist-booking-fyyur/forms.py
+++ artist-booking-fyyur/forms.py
@@ -83,7 +83,11 @@ class VenueForm(Form):
         'address', validators=[DataRequired()]
     )
     phone = StringField(
-        'phone'
+        def validate_phone(self, phone):
+        us_phone_num = '^([0-9]{3})[-][0-9]{3}[-][0-9]{4}$'
+        match = re.search(us_phone_num, phone.data)
+        if not match:
+            raise ValidationError('Error, phone number must be in format xxx-xxx-xxxx')
     )
     image_link = StringField(
         'image_link'
